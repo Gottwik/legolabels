2cb539012498f881b9a9f52241dfa48fb1d1caea
Martin Gottweis <gottwik@gmail.com>
2017-01-25 12:10:29 +0100

    recovered cms folder

---
+++ cms/generators/blog/blog.js
@@ -0,0 +1,23 @@
+{
+	title: '',
+	$date_value: '2016-12-22',
+	$date_type: 'date',
+	date: 'Thursday, 22 December, 2016',
+	published: false,
+	$doc_markdown: true,
+	$doc_type: 'textarea',
+	doc: '',
+	teaser: {
+		doc: '',
+		$abstracted_content_hidden: true,
+		abstracted_content: {
+			marked_doc: '',
+			contents: []
+		}
+	},
+	$abstracted_content_hidden: true,
+	abstracted_content: {
+		marked_doc: '',
+		contents: []
+	}
+}
